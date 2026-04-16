18eaa9497efb2ddf23c977912a2809aaa619d5ab
jon-renzil-08 <johnirevormasiziliwu@gmail.com>
2026-04-16 17:04:50 +0800

    feat: add font awesome cdn to landing page

--- resources/views/components/landing-layout.blade.php
+++ resources/views/components/landing-layout.blade.php
@@ -17,6 +17,7 @@
         href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.4/dist/aos.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
